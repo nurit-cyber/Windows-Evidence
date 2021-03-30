62e1895e4abd383487ba50ad26626fa491a8ddf2
nurit-cyber <54545478+nurit-cyber@users.noreply.github.com>
2021-03-30 12:59:01 -0400

    Update USBWindowsRegistry.ps1

--- USBWindowsRegistry.ps1
+++ USBWindowsRegistry.ps1
@@ -1,2 +1,7 @@
+# RUN AS ADMINISTRATOR
+
 $Path = 'HKLM:\System\CurrentControlSet\Enum\USBStor\*\*'
 Get-ItemProperty -Path $Path | Select-Object -Property PSChildName, FriendlyName, CompatibleIDs, Mfg
+
+# PSChildName is the unique serial number for the drive
+# FriendlyName is the readable name that states the Manufacturer and USB Type
